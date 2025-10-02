65702676c4bdea72591f39eae3ccbc598c107641
jindalhariom86 <jindalhariom86@gmail.com>
2025-10-02 14:44:42 +0530

    Update SOD.c

--- SOD.c
+++ SOD.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
-int main ()
+int main()
 {
+    int digit, remainder, sum;
+
+    sum = 0;
+
     printf("Enter any digit : ");
-    int digit;
-    int remainder;
-    int sum = 0;
-    scanf("%d", &digit);  
-    while (digit!=0)
+    scanf("%d", &digit);
+
+    while (digit != 0)
     {
-        remainder = digit%10;
+        remainder = digit % 10;
         sum = sum + remainder;
-        digit = digit/10;
+        digit = digit / 10;
     }
+
     printf("%d\n", sum);
-    
+
     return 0;
 }
