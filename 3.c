2cb57fc7eb03613fde75bde8aefe219d31515a46
jindalhariom86 <jindalhariom86@gmail.com>
2025-10-02 13:56:59 +0530

    circle.c

--- 3.c
+++ 3.c
@@ -1,17 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
     // Circumference and Area Calculator
+    float r, circum, Area;
 
     printf("Here is Your Favourite Circumference and Area Calculator, But before That you need to Enter Radius of that Circle \n\n");
+
     printf("Please enter Your Radius Here : ");
-    float r;
     scanf("%f", &r);
-    printf("Your Given Radius is : %.2f\n\n",r);
-    float circum = r*3.14*2;
-    float Area = 3.14*r*r;
-    printf("Here is your Circumference of the Cricle : %.2f \n And the Area of the Circle is : %.2f \n\n", circum, Area);
-    printf("Thanks for Using our Calculator");
+
+    printf("Your Given Radius is : %.2f\n\n", r);
     
+    circum = r * 3.14 * 2;
+    Area = 3.14 * r * r;
+    
+    printf("Here is your Circumference of the Cricle : %.2f \nAnd the Area of the Circle is : %.2f \n\n", circum, Area);
+
     return 0;
 }
