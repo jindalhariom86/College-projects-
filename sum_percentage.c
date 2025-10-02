b32b19360f0e8b1a49fc726a4df00fa9547e85d7
jindalhariom86 <jindalhariom86@gmail.com>
2025-10-02 12:20:53 +0530

    Add files via upload

--- sum_percentage.c
+++ sum_percentage.c
@@ -1,53 +1,34 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    // Heading 
+    int Physics_marks, Chemistry_marks, Maths_marks, English_marks, EVS_marks, sum;
+    float Final_percentage;
+
     printf("\n\nEnter subject marks below\n\n");
 
-    //Getting Physics marks
-    printf("Enter markes of Physics : ");
-    int Physics_marks;
-    scanf("%d",&Physics_marks);
-    
-    //Getting Chemistry marks
-    printf("Enter markes of Chemistry : ");
-    int Chemistry_marks;
-    scanf("%d", &Chemistry_marks);
-    
-    //Getting Maths marks
-    printf("Enter marks of Maths : ");
-    int Maths_marks;
-    scanf("%d",&Maths_marks);
-    
-    //Getting English marks
-    printf("Enter marks of English : ");
-    int English_marks;
-    scanf("%d",&English_marks);
-    
-    //Getting EVS marks
-    printf("Enter marks  of EVS : ");
-    int EVS_marks;
-    scanf("%d",&EVS_marks);
-    
-    //Percentage Formula
-    float Final_percentage = (Physics_marks+Chemistry_marks+Maths_marks+EVS_marks+English_marks)/5;
-    
 
-    // Sum here
-    
-    int sum = Maths_marks+EVS_marks+English_marks+Chemistry_marks+Physics_marks;
-    if (sum>500)
-    {
-        printf("You Entered Invalid Numbers\n");
-    }
-    else
-    {
-        printf("\nyour total marks out of 500 : %d\n", sum); 
-    }
-    
-    //Percentage
-    printf("\nYour Final Percentage is : %d%%\n\n", Final_percentage);  
-    
+    printf("Enter marks of Physics : ");
+    scanf("%d", &Physics_marks);
+
+    printf("Enter marks of Chemistry : ");
+    scanf("%d", &Chemistry_marks);
+
+    printf("Enter marks of Maths : ");
+    scanf("%d", &Maths_marks);
+
+    printf("Enter marks of English : ");
+    scanf("%d", &English_marks);
+
+    printf("Enter marks  of EVS : ");
+    scanf("%d", &EVS_marks);
+
+    sum = Maths_marks + EVS_marks + English_marks + Chemistry_marks + Physics_marks;
+
+    printf("\nyour total marks out of 500 : %d\n", sum);
+
+    Final_percentage = sum / 5.0;
+
+    printf("\nYour Final Percentage is : %.2f%%\n\n", Final_percentage);
 
     return 0;
 }
