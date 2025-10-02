a19839f6b2bb4e1b603ccfde37b5568be8766c7b
jindalhariom86 <jindalhariom86@gmail.com>
2025-10-02 13:59:35 +0530

    temperature.c

--- 4.c
+++ 4.c
@@ -2,19 +2,20 @@
 int main()
 {
     // Here is your Centigrade to Fahrenite Converter
+    
+    float centi, fahr;
     printf("\n\nHere is your Centigrade to Fahrenite Converter\n\n");
 
     printf("To convert Centigrade to Fahrenite we need Temprature in Centigrade\n");
-    
+
     printf("Please enter temprature in Centigrade : ");
-    
-    float centi;
     scanf("\n%f", &centi);
-    
+
     printf("Given Temprature is %.2f%cC\n", centi, 248);
     
-    float fahr = (centi * 9 / 5) + 32;
-    
+    fahr = (centi * 9 / 5) + 32;
+
     printf("Here is your Temprature in Fahrenite : %.2f%cF\n\n", fahr, 248);
+    
     return 0;
 }
