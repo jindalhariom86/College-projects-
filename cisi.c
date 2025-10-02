5e1b6037ad7eaf4165ef8435f45b9207ef4b8fcc
jindalhariom86 <jindalhariom86@gmail.com>
2025-10-02 13:46:28 +0530

    Add files via upload

--- cisi.c
+++ cisi.c
@@ -3,52 +3,65 @@
 #include <windows.h>
 int main()
 {
-    printf("Enter 'c' to Calculate Compound Interest and 's' for Simple Interest : ");
     char i;
+    float prin, rate, time, amount;
+
+    printf("Enter 'c' to Calculate Compound Interest and 's' for Simple Interest : ");
+
     scanf("%c", &i);
     if (i == 'c')
     {
         //  Compound Interest Calculator
 
         SetConsoleOutputCP(CP_UTF8);
-        printf("\nHere is you favourite Coumpond Interest Calculator\n\n");
+        printf("\nHere is your favourite Coumpond Interest Calculator\n\n");
+
+
         printf("Please Enter your Principal Amount here (₹) : ");
-        float prin;
         scanf("%f", &prin);
+        
         printf("Now Enter Rate in Percentage (%%): ");
-        float rate;
         scanf("%f", &rate);
+        
         printf("Enter your Time in Years : ");
-        float time;
         scanf("%f", &time);
-        printf("Given Principal Amount is (₹): %.2f \n Given Rate is : %.2f %%\n Given Time in Years is : %.2f", prin, rate, time);
-        float amo = prin * pow((1 + (rate / 100)), time);
-        printf("\n\nYour Total Amount after %.2f Year : %.2f\n\n", time, amo);
-        printf("\nHere is your Compound Interest : %.2f", amo - prin);
+        
+        printf("\nGiven Principal Amount is (₹): %.2f \n Given Rate is : %.2f %%\n Given Time in Years is : %.2f", prin, rate, time);
+        amount = prin * pow((1 + (rate / 100)), time);
+        
+        printf("\nHere is your Compound Interest : %.2f", amount - prin);
+
+        printf("\n\nYour Total Amount after %.2f Year : %.2f\n\n", time, amount);
     }
-    //Simple Intrest
-    
+
     else if (i == 's')
     {
+        // Simple Intrest
         SetConsoleOutputCP(CP_UTF8);
         printf("\nHere is you favourite Simple Interest Calculator\n\n");
+        
+        
         printf("Please Enter your Principal Amount here (₹) : ");
-        float prin;
         scanf("%f", &prin);
+        
         printf("Now Enter Rate in Percentage (%%): ");
-        float rate;
         scanf("%f", &rate);
+        
         printf("Enter your Time in Years : ");
-        float time;
         scanf("%f", &time);
+        
         printf("Given Principal Amount is (₹): %.2f \n Given Rate is : %.2f %%\n Given Time in Years is : %.2f", prin, rate, time);
-        float amo = (prin*rate*time)/100;
-        printf("\n\nYour Total Amount after %.2f Year : %.2f\n\n", time, amo+prin);
-        printf("\nHere is your Simple Interest : %.2f", amo);
+        amount = (prin * rate * time) / 100;
+        
+        printf("\nHere is your Simple Interest : %.2f", amount);
+
+        printf("\n\nYour Total Amount after %.2f Year : %.2f\n\n", time, amount + prin);        
     }
+
     else
     {
         printf("Please enter 'C' for Compuond Interest and 'S' for Simple Interest\n");
     }
+
     return 0;
 }
