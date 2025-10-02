7bf62ffa78e45733e9003907fe2904846f659c1e
jindalhariom86 <jindalhariom86@gmail.com>
2025-10-02 14:37:43 +0530

    Update sum_of_all_given_numbers.c

--- sum_of_all_given_numbers.c
+++ sum_of_all_given_numbers.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 int main()
 {
-    printf("Enter how many numbers you want to Add : ");
     int numbers;
+    float sum;
+
+    printf("Enter how many numbers you want to Add : ");
     scanf("%d", &numbers);
+
     float all_numbers[numbers];
-    float sum = 0;
+    sum = 0;
+
     for (int i = 0; i < numbers; i++)
-    {        
-        printf("Enter %dst Number : ", i+1);
+    {
+        printf("Enter %dst Number : ", i + 1);
         scanf("%f", &all_numbers[i]);
-        sum += all_numbers[i];        
+
+        sum += all_numbers[i];
     }
-    printf("Here is the sum of all given Numbers : %f", sum);
-    
+
+    printf("Here is the sum of all given Numbers : %.2f", sum);
+
     return 0;
 }
